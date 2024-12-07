f169e60f7e41da4986bd65b46f2b08e3b4467546
Ivan <aw4ken1ngs@gmail.com>
2024-12-08 01:13:19 +0400

    work in progress

--- src/components/clouds/clouds.jsx
+++ src/components/clouds/clouds.jsx
@@ -7,27 +7,23 @@ import cloudImg5 from "../../img/cloud5-img.png";
 import cloudImg6 from "../../img/cloud6-img.png";
 import cloudImg7 from "../../img/cloud7-img.png";
 
+const CLOUDS = [
+    { id: 1, src: cloudImg4, className: styles.animationCloud1, width: 400, height: 100 },
+    { id: 2, src: cloudImg5, className: styles.animationCloud2, width: 400, height: 100 },
+    { id: 3, src: cloudImg2, className: styles.animationCloud4, width: 250, height: 125 },
+    { id: 4, src: cloudImg, className: styles.animationCloud5, width: 350, height: 175 },
+    { id: 5, src: cloudImg6, className: styles.animationCloud6, width: 450, height: 225 },
+    { id: 6, src: cloudImg7, className: styles.animationCloud7, width: 450, height: 225 },
+];
+
 export function Clouds() {
     return (
         <>
-            <div className={styles.animationCloud1}>
-                <img src={cloudImg4} alt="image cloud" width="400" height="100"/>
-            </div>
-            <div className={styles.animationCloud2}>
-                <img src={cloudImg5} alt="image cloud" width="400" height="100"/>
-            </div>
-            <div className={styles.animationCloud4}>
-                <img src={cloudImg2} alt="image cloud" width="250" height="125"/>
-            </div>
-            <div className={styles.animationCloud5}>
-                <img src={cloudImg} alt="image cloud" width="350" height="175"/>
-            </div>
-            <div className={styles.animationCloud6}>
-                <img src={cloudImg6} alt="image cloud" width="450" height="225"/>
-            </div>
-            <div className={styles.animationCloud7}>
-                <img src={cloudImg7} alt="image cloud" width="450" height="225"/>
-            </div>
+            {CLOUDS.map((cloud) => (
+                <div key={cloud.id} className={cloud.className}>
+                    <img src={cloud.src} alt="image cloud" width={cloud.width} height={cloud.height} />
+                </div>
+            ))}
         </>
-    )
+    );
 }
